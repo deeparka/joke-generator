48f02b2956dec312000cb6bf70f830153d9a51aa
deeparka <arkadeep180@gmail.com>
2022-05-26 11:40:48 +0530

    completed the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useRef, useState } from "react";
 import useRandomJoke from "./Custom Hook/useRandomJoke";
 
 function App() {
-    const joke = useRandomJoke();
+    const firstNameRef = useRef(null);
+    const lastNameRef = useRef(null);
+
+    const [firstName, setFirstName] = useState("John");
+    const [lastName, setLastName] = useState("Doe");
+
+    const joke = useRandomJoke(firstName, lastName);
+
+    const generateJoke = (e) => {
+        e.preventDefault();
+        setFirstName(firstNameRef.current.value);
+        setLastName(lastNameRef.current.value);
+    };
+
     return (
         <div>
             <center>
                 <h1>The Joke Generator</h1>
                 <h5>{joke}</h5>
-                <button>Generate Joke</button>
+                <form action="">
+                    <input placeholder="First Name" ref={firstNameRef} />
+                    <input placeholder="Last Name" ref={lastNameRef} />
+                    <button onClick={generateJoke}>Generate Joke</button>
+                </form>
             </center>
         </div>
     );
--- src/Custom Hook/useRandomJoke.jsx
+++ src/Custom Hook/useRandomJoke.jsx
@@ -1,19 +1,20 @@
 import { useState, useEffect } from "react";
 
-function useRandomJoke() {
+function useRandomJoke(firstName, lastName) {
     const [joke, setJoke] = useState(``);
 
     useEffect(() => {
         const generateJoke = async () =>
             await fetch(
-                `http://api.icndb.com/jokes/random?firstName=Arkadeep&lastName=Prasad`
+                `http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`
             )
                 .then((response) => response.json())
                 .then((data) => {
                     setJoke(data.value.joke);
                 });
+
         generateJoke();
-    }, []);
+    }, [firstName, lastName]);
 
     return joke;
 }
